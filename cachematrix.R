f15f6136b53b6297cc3f9cc4a4fc2dcc62107f95
zhou00yu <liz150@ucsd.edu>
2020-07-12 16:36:12 -0700

    Add one test matrix

--- cachematrix.R
+++ cachematrix.R
@@ -41,3 +41,29 @@ cacheSolve <- function(x, ...) {
   x$setinv(m)
   return(m)
 }
+
+
+source("./cachematrix.R")
+
+## test makeCacheMatrix
+test_mat <- makeCacheMatrix(matrix(5:8, 2, 2))
+test_mat$get()
+# [,1] [,2]
+# [1,]    5    7
+# [2,]    6    8
+
+test_mat$getinv()
+# NULL
+
+## Test for cacheSolve: after finding the inverse using casheSolve, 
+# then the same inverse of the matrix will be the output of getinv()
+cacheSolve(test_mat)
+#       [,1] [,2]
+# [1,]   -4  3.5
+# [2,]    3 -2.5
+
+test_mat$getinv()
+
+# [,1] [,2]
+# [1,]   -4  3.5
+# [2,]    3 -2.5
